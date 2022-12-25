05e3630eaf28b9646c109945e02c7ef78ff6e76a
Kadinata <2396847+Kadinata@users.noreply.github.com>
2022-12-25 17:28:10 -0600

    Adding leet code links

--- leet_code_2095.py
+++ leet_code_2095.py
@@ -1,6 +1,9 @@
 '''
 LeetCode 2095: Delete the Middle Node of a Linked List (Medium)
 
+Link:
+https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+
 Problem:
 You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
 The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, 
